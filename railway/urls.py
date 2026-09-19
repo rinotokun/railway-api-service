bd857335be9b46f3dfa2d65399f2e3c99e51e15c
Sergey <rinoto_kun@gmail.com>
2026-09-19 19:02:13 +0300

    Add railway API endpoints with serializers and viewsets

--- railway/urls.py
+++ railway/urls.py
@@ -1,6 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from railway.views import (
+    StationViewSet,
+    RouteViewSet,
+    TrainTypeViewSet,
+    TrainViewSet,
+    CrewViewSet,
+    JourneyViewSet,
+    OrderViewSet,
+)
 
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("train_types", TrainTypeViewSet)
+router.register("train", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("journeys", JourneyViewSet)
+router.register("orders", OrderViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "railway"
--- railway/views.py
+++ railway/views.py
@@ -1,3 +1,106 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from railway.models import (
+    Station,
+    Route,
+    TrainType,
+    Train,
+    Crew,
+    Journey,
+    Order
+)
+
+from railway.serializers import (
+    StationSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    TrainTypeSerializer,
+    TrainSerializer,
+    TrainListSerializer,
+    CrewSerializer,
+    JourneySerializer,
+    JourneyListSerializer,
+    JourneyDetailSerializer,
+    OrderSerializer,
+    OrderListSerializer,
+    OrderDetailSerializer,
+)
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return RouteListSerializer
+
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+
+        return RouteSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.action in ("list", "retrieve"):
+            return TrainListSerializer
+
+        return TrainSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return JourneyListSerializer
+
+        if self.action == "retrieve":
+            return JourneyDetailSerializer
+
+        return JourneySerializer
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    pagination_class = OrderPagination
+
+    def get_serializer_class(self):
+
+        if self.action == "list":
+            return OrderListSerializer
+
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
