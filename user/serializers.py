1bb5d755818bd879409aad255bb8cf62ddbae4dd
Sergey <rinoto_kun@gmail.com>
2026-09-24 19:25:36 +0300

    Mask the password field in the browsable API

--- user/serializers.py
+++ user/serializers.py
@@ -7,7 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "username", "password", "is_staff")
         read_only_fields = ("is_staff",)
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"}
+            }
+        }
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
